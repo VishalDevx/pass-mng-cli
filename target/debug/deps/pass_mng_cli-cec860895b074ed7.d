e777c7f7114b9eac9aab1f81fc09438821644df3
root <root@DESKTOP-U1KNI1C.localdomain>
2025-07-14 11:04:20 +0530

    start to building the pass manager cli

---
+++ target/debug/deps/pass_mng_cli-cec860895b074ed7.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/pc/Desktop/Code PlayGround/rust-proj/pass-mng-cli/target/debug/deps/pass_mng_cli-cec860895b074ed7.d: src/main.rs
+
+/mnt/c/Users/pc/Desktop/Code PlayGround/rust-proj/pass-mng-cli/target/debug/deps/pass_mng_cli-cec860895b074ed7: src/main.rs
+
+src/main.rs:
---
+++ target/debug/pass-mng-cli.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/pc/Desktop/Code\ PlayGround/rust-proj/pass-mng-cli/target/debug/pass-mng-cli: /mnt/c/Users/pc/Desktop/Code\ PlayGround/rust-proj/pass-mng-cli/src/main.rs
