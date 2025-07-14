4b2eda7ba77e441a50b3206f14e69d639745fa9a
1VishalSD <ilearnvk@gmail.com>
2025-07-15 05:05:20 +0530

    just trying the understand the rust code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
-}
+#[macro_use]
+ extern crate magic_crypt;
+ extern  crate passwords;
+ use std::{collections::HashMap, fs};
+
+
+
+
+ fn load_data() -> HashMap<String, String> {
+	let data::String = fs::read_to_string("db.json").unwrap_or_else(|_|  "{}".to_string());
+    serde_json::from_str(&data).unwrap_or_default()
+ }
